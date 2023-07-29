5a69d7c2bd2230934be196723bb7ed4e5459123f
Yeyo8711 <jair.carmona87@gmail.com>
2023-07-28 19:06:42 -0600

    UPDATED

--- index.js
+++ index.js
@@ -3,29 +3,62 @@ const { ethers } = require("ethers");
 
 // CONTRACT STUFF
 const abi = require("./abi.json");
-const contractAddress = "0xC690b21314E14649A1B01F3B6452a84Ab692140e"; // REPLACE WITH CORRECT ADDRESS
+const contractAddress = "0xcE973c7f15b7607c3320D45F302a69FD5E59EbCa"; // REPLACE WITH CORRECT ADDRESS
 // ETHERS STUFF
 const provider = new ethers.providers.JsonRpcProvider(
   "https://rpc.ankr.com/polygon"
 );
-const wallet = new ethers.Wallet(
-  "", // UPDATE WITH YOUR PRIVATE KEY
-  provider
-);
+const wallet = new ethers.Wallet("", provider); // REPLACE WITH PRIVATE KEY
 const RecipientNFT8 = new ethers.Contract(contractAddress, abi, wallet);
 
 // WALLETS
 const addresses = require("./wallets.json");
+const batchSize = 500; // AMOUNT PER BATCH
+const numberOfBatches = Math.ceil(addresses.length / batchSize);
 
 const main = async () => {
-  try {
-    const airdrop = await RecipientNFT8.airdrop1(addresses);
-    await airdrop.wait();
+  for (let i = 0; i < numberOfBatches; i++) {
+    const startIndex = i * batchSize;
+    const endIndex = Math.min(startIndex + batchSize, addresses.length);
+    const addressesBlock = addresses.slice(startIndex, endIndex);
+
+    try {
+      const gasPrice = await provider.getGasPrice();
+      const formattedGas = ethers.utils.formatUnits(gasPrice, "gwei");
+      console.log(
+        `Minting ${startIndex} - ${endIndex} with ${formattedGas} gas`
+      );
+
+      const airdrop = await RecipientNFT8.airdrop1(addressesBlock, {
+        gasPrice,
+      });
+      await airdrop.wait();
+      console.log(
+        `Successfull transaction https://polygonscan.com/${airdrop.hash}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Handle the remaining addresses after processing the blocks of 500 addresses
+  const remainingAddresses = addresses.slice(numberOfBatches * batchSize);
+  if (remainingAddresses.length > 0) {
     console.log(
-      `Successfull transaction https://polygonscan.com/${airdrop.hash}`
+      `Processing remaining ${remainingAddresses.length} addresses...`
     );
-  } catch (error) {
-    console.log(error);
+
+    try {
+      console.log(`Minting...`);
+
+      const airdrop = await RecipientNFT8.airdrop1(remainingAddresses);
+      await airdrop.wait();
+      console.log(
+        `Successfull transaction https://polygonscan.com/${airdrop.hash}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
 
